aa2f0714762db87abf07e9ca6671642dc7d61270
0-V-linuxdo <0_v@linux.do>
2026-09-13 09:14:23 +0800

    [20260912.46] restore build.ts and use Grok SettingsRow

--- build.ts
+++ build.ts
@@ -15,3 +15,4 @@ const environment = isDev ? "Development" : "Production";
 const FORK_URL = "https://github.com/0-V-linuxdo/VoidPP";
 const SCRIPT_CDN = "https://raw.githubusercontent.com/0-V-linuxdo/VoidPP/voidpp";
 const VERSION_DATE = "20260912.46";
+const displayVersion = `[${VERSION_DATE}] v${pkg.version}`;
